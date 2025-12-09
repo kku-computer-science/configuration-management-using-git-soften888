0bbfeec9046b909f3ccd44b72acd62be6716818e
nattaphat010447 <jangkup90@gmail.com>
2025-12-09 16:50:47 +0700

    update staging

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import Project.BubbleSort as BubbleSort
 import Project.quicksort as QuickSort
 
-array = list(map(int, input("ป้อนลำดับตัวเลขที่ต้องการจัดเรียง: ").split()))
+array = list(map(int, input("ป้อนลำดับตัวเลขที่ต้องการจัดเรียง : ").split()))
 print("เลือกวิธีการเรียงลำดับ\n1. Quick Sort\n2. Bubble Sort")
 
 choice = input("ป้อนตัวเลือก: ")
