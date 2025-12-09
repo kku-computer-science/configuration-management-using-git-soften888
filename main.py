b75812a872ff26cd2b5cc4f8e83bf8c8e520d37c
nattaphat010447 <jangkup90@gmail.com>
2025-12-09 16:24:34 +0700

    Fix type conversion in sorting result output

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ print("เลือกวิธีการเรียงลำดับ\n1. Qu
 choice = input("ป้อนตัวเลือก: ")
 if choice == "1":
     result = BubbleSort.bubble_sort(array)
-    print("Bubble Sort Result : " + result)
+    print("Bubble Sort Result : " + str(result))
 
 elif choice == "2":
     result = QuickSort.quick_sort(array)
-    print("Quick Sort Result : " + result)
+    print("Quick Sort Result : " + str(result))
 
 else:
     print("ตัวเลือกไม่ถูกต้อง")
