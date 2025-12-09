8d59b0e4f663256c611816ee9baed4e93ba8e278
nattaphat010447 <jangkup90@gmail.com>
2025-12-09 16:06:15 +0700

    Refactor sorting options to implement actual sorting algorithms for user selection

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+import Project.BubbleSort as BubbleSort
+import Project.quicksort as QuickSort
+
 array = list(map(int, input("ป้อนลำดับตัวเลขที่ต้องการจัดเรียง: ").split()))
 print("เลือกวิธีการเรียงลำดับ\n1. Quick Sort\n2. Bubble Sort")
 
 choice = input("ป้อนตัวเลือก: ")
 if choice == "1":
-    result = array
-    #ทำการ เรียงลำดับด้วย Quick Sort
-    print(result)
+    result = BubbleSort.bubble_sort(array)
+    print("Bubble Sort Result : " + result)
 
 elif choice == "2":
-    result = array
-    #ทำการ เรียงลำดับด้วย Bubble Sort
-    print(result)
+    result = QuickSort.quick_sort(array)
+    print("Quick Sort Result : " + result)
 
 else:
     print("ตัวเลือกไม่ถูกต้อง")
